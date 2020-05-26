7a1da3dd6d0135bf2367adb0875276bd49566e04
Alexander Malygin <limanka92@gmail.com>
2020-05-26 12:21:22 +0300

    Upload app

---
+++ MacMini/MenuCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCollectionViewCell.swift
+//  MacMini
+//
+//  Created by Alexader Malygin on 25.05.2020.
+//  Copyright © 2020 Alexader Malygin. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MacMini/ProductViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacMini
+//
+//  Created by Alexader Malygin on 25.05.2020.
+//  Copyright © 2020 Alexader Malygin. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
